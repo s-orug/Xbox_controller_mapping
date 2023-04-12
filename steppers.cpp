dcb997f9b528476bd4ecd4a493ffa03ca51cb5c7
s-orug <orugants@email.sc.edu>
2023-04-12 16:23:57 -0400

    update

--- steppers.cpp
+++ steppers.cpp
@@ -25,30 +25,30 @@ void setup() {
 }
 
 // Function to control the motor 1
-void motor1(int direction, int steps) {
+void motor1(int direction, int steps, int delay) {
     // Set the direction pin based on the input
     digitalWrite(M1_DIR_PIN, direction);
 
     // Step the motor the specified number of times
     for (int i = 0; i < steps; i++) {
         digitalWrite(M1_STEP_PIN, HIGH);
-        delayMicroseconds(STEP_DELAY_MICROSECONDS);
+        delayMicroseconds(delay);
         digitalWrite(M1_STEP_PIN, LOW);
-        delayMicroseconds(STEP_DELAY_MICROSECONDS);
+        delayMicroseconds(delay);
     }
 }
 
 // Function to control the motor 2
-void motor2(int direction, int steps) {
+void motor2(int direction, int steps, int delay) {
     // Set the direction pin based on the input
     digitalWrite(M2_DIR_PIN, direction);
 
     // Step the motor the specified number of times
     for (int i = 0; i < steps; i++) {
         digitalWrite(M2_STEP_PIN, HIGH);
-        delayMicroseconds(STEP_DELAY_MICROSECONDS);
+        delayMicroseconds(delay);
         digitalWrite(M2_STEP_PIN, LOW);
-        delayMicroseconds(STEP_DELAY_MICROSECONDS);
+        delayMicroseconds(delay);
     }
 }
 
@@ -57,8 +57,8 @@ int main() {
     setup();
 
     // Start two threads to control the motors simultaneously
-    std::thread t1(motor1, 1, 200);  // clockwise
-    std::thread t2(motor2, 0, 200);  // counterclockwise
+    std::thread t1(motor1, 1, 2000, 1500);  // clockwise
+    std::thread t2(motor2, 0, 2000, 1500);  // counterclockwise
 
     // Wait for both threads to finish
     t1.join();
