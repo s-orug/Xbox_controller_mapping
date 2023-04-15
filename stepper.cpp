32aa24ec92f884a27b6bf276aef2a61fc30cc69b
s-orug <orugants@email.sc.edu>
2023-04-14 21:06:56 -0400

    stepper with variable speed

--- stepper.cpp
+++ stepper.cpp
@@ -1,44 +1,75 @@
 #include <wiringPi.h>
+#include <cmath>
 #include <iostream>
 
-// Define pins for the motor
-//                         W  B 
-const int STEP_PIN = 19; //16 19  GPIO17 (WiringPi pin 0)
-const int DIR_PIN = 26;  //13 26  GPIO27 (WiringPi pin 2)
+#define MIN_VELOCITY_THRESHOLD 1e-6
+#define MAX_STEP_DELAY 1e6
+#define STEP_PULSE_WIDTH 1e-6
 
-// Define stepper motor parameters
-const int STEPS_PER_REV = 200;   // Steps per revolution
-const int MICROSTEPS = 1;       // Microsteps per step
-const float STEP_DELAY = 2000.0;  // Delay in microseconds between each step
+class Stepper {
+private:
+    int _dir_pin, _step_pin, _ppr;
+    int position = 0;
+    int dx = 1;
+    float step_delay = 0;
+    float last_step_ts = 0;
 
-int main() {
-    // Initialize WiringPi library
-    wiringPiSetupGpio();
-
-    // Configure motor pins as output
-    pinMode(STEP_PIN, OUTPUT);
-    pinMode(DIR_PIN, OUTPUT);
-
-    // Set the motor direction (CW or CCW)
-    digitalWrite(DIR_PIN, HIGH); // CW
-
-    // Move the motor one revolution
-    for (int i = 0; i < STEPS_PER_REV * MICROSTEPS; i++) {
-        digitalWrite(STEP_PIN, HIGH);
-        delayMicroseconds(STEP_DELAY);
-        digitalWrite(STEP_PIN, LOW);
-        delayMicroseconds(STEP_DELAY);
+public:
+  int CW = 1;
+  int CCW = 0;
+    Stepper(int dir_pin, int step_pin, int ppr){
+        wiringPiSetupGpio();
+        pinMode(dir_pin, OUTPUT);
+        pinMode(step_pin, OUTPUT);
+	_dir_pin = dir_pin;
+	_step_pin = step_pin;
+	_ppr = ppr;
     }
 
-    // Set the motor direction (CCW or CW)
-    digitalWrite(DIR_PIN, LOW); // CCW
+    void set_direction(int direction) {
+        digitalWrite(_dir_pin, direction);
+        if (direction == CW) {
+            dx = 1;
+        } else {
+            dx = -1;
+        }
+    }
 
-    // Move the motor one revolution
-    for (int i = 0; i < STEPS_PER_REV * MICROSTEPS; i++) {
-        digitalWrite(STEP_PIN, HIGH);
-        delayMicroseconds(STEP_DELAY);
-        digitalWrite(STEP_PIN, LOW);
-        delayMicroseconds(STEP_DELAY);
+    void step() {
+        digitalWrite(_step_pin, HIGH);
+        delay(STEP_PULSE_WIDTH);
+        digitalWrite(_step_pin, LOW);
+        position += dx;
+    }
+
+    void set_velocity(double velocity) {
+        if (std::abs(velocity) < MIN_VELOCITY_THRESHOLD) {
+            step_delay = MAX_STEP_DELAY;
+        } else {
+	  step_delay = (((2.0 * M_PI) / _ppr) / abs(velocity)) - (1e-6);
+            set_direction(velocity > 0 ? CW : CCW);
+        }
+	std::cout << step_delay << std::endl ;
+    }
+
+    void loop() {
+        float now = micros() / 1e6;
+	if (now - last_step_ts >= step_delay) {
+            step();
+            last_step_ts = now;
+	}  
+    }
+};
+
+int main() {
+    wiringPiSetupGpio();
+    Stepper motor(13, 6, 200);
+
+    motor.set_direction(1);
+    motor.set_velocity(-10 * M_PI);
+
+    while (true) {
+        motor.loop();
     }
 
     return 0;
